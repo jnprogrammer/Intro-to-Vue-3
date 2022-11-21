3048a9a1f635e8cc533be74f6c39b5bc82b44d5d
jnprogrammer <joshuanaprker@gmail.com>
2022-11-20 19:27:51 -0500

    code solved

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'liquid',
+            image: './assets/images/funky.png',
+            description: "Liquid that's pretty funky",
+            url: "https://pool.pm/asset1wysvkmqxwzqpxzwnxhr6te3xj882vn84nzmyzx?wsort=dec"
         }
     }
 })
